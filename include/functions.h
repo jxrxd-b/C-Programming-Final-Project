b892713546289f268230ad1a7cbb2eb833703687
Jonathan-1738 <jpaessen@gmail.com>
2025-04-21 21:47:36 -0400

    typedef stuff

--- include/functions.h
+++ include/functions.h
@@ -5,16 +5,16 @@
 #include <string.h>
 
 
-struct Contact {
+typedef struct Contact {
 	char name[50];
 	char phoneNum[15];
 	int age;
-};
+}Contact;
 
-struct Phonebook {
+typedef struct Phonebook {
 	struct Contact contacts[100];
 	int count;
-};
+}PhoneBook;
 
 addContact(char* name, char* phoneNum);
 
