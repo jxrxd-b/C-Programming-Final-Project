5699eb8dbc9eaa66b85ddb2f9be9cce989ba4b75
Jonathan-1738 <jpaessen@gmail.com>
2025-04-23 22:32:14 -0400

    4/23 10:32

--- src/main.c
+++ src/main.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
 #include "../include/functions.h"
 
+
+/*
+* Name:				main()
+* Parameters:		none
+* Processes:		runs the phonebook program
+* Return Value: 	0
+*/
 int main() {
-	PhoneBook phonebook;
-	phonebook.count = 0;
-	char choice;
-	printf("Welcome to the phone book!\n\n");
-	do {
-		choice = getPhonebookMenuChoice();
-		setPhonebookMenu(choice, &phonebook);
-	} while (choice != 5);
-	return 0;
+    PhoneBook phonebook;
+    phonebook.count = 0;
+    int choice;
+    do {
+        choice = getPhonebookMenuChoice();
+        setPhonebookMenu(choice, &phonebook);
+    } while (choice != 6);
+    return 0;
 }
