cb71035bb3fef03b230b6214f741dce176a4aaac
Jonathan-1738 <jpaessen@gmail.com>
2025-04-23 22:33:40 -0400

    again.2

--- include/functions.h
+++ include/functions.h
@@ -8,19 +8,8 @@ typedef struct Contact {
 	char firstName[50];
 	char lastName[50];
 	int age;
-
 	char phoneNum[15];
 
-	char city[50];
-	char state[50];
-	char country[50];
-	char adress[100];
-	int zipCode;
-
-	char email[50];
-
-	char notes[100];   // 100 character limit (if too comolex, we'll get rid of this)
-	char birthday[11]; // YYYY-MM-DD
 
 } Contact;
 
@@ -29,33 +18,20 @@ typedef struct Phonebook {
 	int count;
 } PhoneBook;
 
-/*
-* Name:				getPhoneBookMenuChoice()
-* Parameters:		none
-* Processes:		displays options and gets a char choice
-* Return Value: 	choice
-*/
+
 
 int getPhonebookMenuChoice();
 
-/*
-* Name:				setPhoneBookMenu
-* Parameters:		choice
-* Processes:		uses Choice to run function options, that add, display, delete, and search contacts
-* Return Value: 	void
-*/
-
 void setPhonebookMenu(int choice, PhoneBook *phonebook);
 
-
-
-
-Contact addContact(PhoneBook *phonebook);
+Contact addContact();
 
 void deleteContact(PhoneBook *pb, char* phoneNum);
 
-searchContacts(char* name, char* phoneNum);
+void searchContacts(char* name, char* phoneNum);
 
 void displayContacts(PhoneBook* pb);
+
+void editContact(PhoneBook* phonebook);
 
 #endif
